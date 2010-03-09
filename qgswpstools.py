66e4617f47c35c116b1b472ea5223b2366358414
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2010-03-09 12:57:47 +0000

    QtSql wieder eingebaut, da fuer das Ermitteln des Encodings der DB wichtig

--- qgswpstools.py
+++ qgswpstools.py
@@ -18,6 +18,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtNetwork import *
 from PyQt4 import QtXml
+from PyQt4.QtSql import *
 from qgis.core import *
 from httplib import *
 from urlparse import urlparse
