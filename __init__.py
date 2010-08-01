93dba3871d05913df953270527df03f62a16466d
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2010-08-01 17:30:44 +0000

    Header eingefuegt

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
 # -*- coding: latin1 -*-
+# /***************************************************************************
+#   qgswps.py QGIS Web Processing Service Plugin
+#  -------------------------------------------------------------------
+# Date                 : 09 November 2009
+# Copyright            : (C) 2009 by Dr. Horst Duester
+# email                : horst dot duester at kappasys dot ch
+#  ***************************************************************************
+#  *                                                                         *
+#  *   This program is free software; you can redistribute it and/or modify  *
+#  *   it under the terms of the GNU General Public License as published by  *
+#  *   the Free Software Foundation; either version 2 of the License, or     *
+#  *   (at your option) any later version.                                   *
+#  *                                                                         *
+#  ***************************************************************************/
 from qgswps import QgsWps
 def name():
   return "WPS-Client"
