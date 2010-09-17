52e95d72038b4e3c1e4fe5bbe652aec0ec939115
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2010-09-17 13:22:09 +0000

    Auf neue Version gesetzt

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def name():
 def description():
   return "Client for Web Processing Services"
 def version():
-  return "Version 0.3.32"
+  return "Version 0.3.34"
 def qgisMinimumVersion():
   return "1.4"  
 def classFactory(iface):
