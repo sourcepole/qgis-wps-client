ab1e579c7c2711628bdaaf93b78cf097e5ac79a0
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2010-12-08 09:36:49 +0000

    Initial Import

---
+++ i18n/wps_de.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="Ui_qgswpserrormsggui.py" line="29"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+</context>
+<context>
+    <name>QgsNewHttpConnectionBase</name>
+    <message>
+        <location filename="Ui_qgsnewhttpconnectionbase.py" line="54"/>
+        <source>Create a new WPS connection</source>
+        <translation>Neue WPS-Verbindung erstellen</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgsnewhttpconnectionbase.py" line="55"/>
+        <source>Connection details</source>
+        <translation>Verbindungsinformationen</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgsnewhttpconnectionbase.py" line="56"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgsnewhttpconnectionbase.py" line="57"/>
+        <source>Name of the new connection</source>
+        <translation>Name der neuen Verbindung</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgsnewhttpconnectionbase.py" line="58"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgsnewhttpconnectionbase.py" line="59"/>
+        <source>HTTP address of the Web Map Server</source>
+        <translation>HTTP Adresse des Web Processing Servers</translation>
+    </message>
+    <message>
+        <location filename="ui_newhttpconnectionbase.py" line="66"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+</context>
+<context>
+    <name>QgsWps</name>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="70"/>
+        <source>Note: this plugin not considered stable yet. Use it on your own risk</source>
+        <translation>Dieses Plugin ist noch nicht stabil. Sie verwendes es auf eigenes Risiko</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="71"/>
+        <source>Server Connections</source>
+        <translation>Server.Verbindungen</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="72"/>
+        <source>&amp;New</source>
+        <translation>Neu</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="73"/>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="74"/>
+        <source>C&amp;onnect</source>
+        <translation>Verbinden</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="75"/>
+        <source>Delete</source>
+        <translation>löschen</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="76"/>
+        <source>about</source>
+        <translation>über</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="78"/>
+        <source>Identifier</source>
+        <translation>Identifier</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="79"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <location filename="Ui_qgswpsgui.py" line="80"/>
+        <source>Abstract</source>
+        <translation>Abstract</translation>
+    </message>
+</context>
+<context>
+    <name>QgsWpsDescribeProcessGUI</name>
+    <message>
+        <location filename="Ui_qgswpsdescribeprocessgui.py" line="21"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+</context>
+</TS>
