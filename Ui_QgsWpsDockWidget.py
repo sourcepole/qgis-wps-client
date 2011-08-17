1c4afae8dd3b3959e344c953e7fad9a5f37e7e93
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2011-08-17 10:02:49 +0000

    connect button eingebaut

--- Ui_QgsWpsDockWidget.py
+++ Ui_QgsWpsDockWidget.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home_nas/sogis/barpadue/.qgis/python/plugins/wps/QgsWpsDockWidget.ui'
 #
-# Created: Wed Aug 17 11:33:31 2011
+# Created: Wed Aug 17 11:51:31 2011
 #      by: PyQt4 UI code generator 4.5.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,7 @@ from PyQt4 import QtCore, QtGui
 class Ui_QgsWpsDockWidget(object):
     def setupUi(self, QgsWpsDockWidget):
         QgsWpsDockWidget.setObjectName("QgsWpsDockWidget")
-        QgsWpsDockWidget.resize(262, 190)
+        QgsWpsDockWidget.resize(267, 190)
         QgsWpsDockWidget.setMinimumSize(QtCore.QSize(190, 190))
         QgsWpsDockWidget.setBaseSize(QtCore.QSize(0, 0))
         font = QtGui.QFont()
@@ -29,6 +29,9 @@ class Ui_QgsWpsDockWidget(object):
         self.warning.setGeometry(QtCore.QRect(30, 50, 161, 20))
         self.warning.setTextFormat(QtCore.Qt.PlainText)
         self.warning.setObjectName("warning")
+        self.btnConnect = QtGui.QPushButton(self.dockWidgetContents)
+        self.btnConnect.setGeometry(QtCore.QRect(20, 10, 78, 27))
+        self.btnConnect.setObjectName("btnConnect")
         QgsWpsDockWidget.setWidget(self.dockWidgetContents)
 
         self.retranslateUi(QgsWpsDockWidget)
@@ -36,6 +39,7 @@ class Ui_QgsWpsDockWidget(object):
 
     def retranslateUi(self, QgsWpsDockWidget):
         QgsWpsDockWidget.setWindowTitle(QtGui.QApplication.translate("QgsWpsDockWidget", "QgsWps", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnConnect.setText(QtGui.QApplication.translate("QgsWpsDockWidget", "connect", None, QtGui.QApplication.UnicodeUTF8))
 
 
 if __name__ == "__main__":
