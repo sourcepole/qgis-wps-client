2757218d1828d80cd7cfedc47c5ebbb07aaaed8a
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2011-08-17 18:17:54 +0000

    grundlegende initialisierung des widgets

--- Ui_QgsWpsDockWidget.py
+++ Ui_QgsWpsDockWidget.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home_nas/sogis/barpadue/.qgis/python/plugins/wps/QgsWpsDockWidget.ui'
+# Form implementation generated from reading ui file '/home/hdus/.qgis/python/plugins/wps/QgsWpsDockWidget.ui'
 #
-# Created: Wed Aug 17 11:51:31 2011
-#      by: PyQt4 UI code generator 4.5.4
+# Created: Wed Aug 17 16:23:58 2011
+#      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,7 +12,7 @@ from PyQt4 import QtCore, QtGui
 class Ui_QgsWpsDockWidget(object):
     def setupUi(self, QgsWpsDockWidget):
         QgsWpsDockWidget.setObjectName("QgsWpsDockWidget")
-        QgsWpsDockWidget.resize(267, 190)
+        QgsWpsDockWidget.resize(211, 224)
         QgsWpsDockWidget.setMinimumSize(QtCore.QSize(190, 190))
         QgsWpsDockWidget.setBaseSize(QtCore.QSize(0, 0))
         font = QtGui.QFont()
@@ -25,21 +25,23 @@ class Ui_QgsWpsDockWidget(object):
         QgsWpsDockWidget.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea)
         self.dockWidgetContents = QtGui.QWidget()
         self.dockWidgetContents.setObjectName("dockWidgetContents")
-        self.warning = QtGui.QLabel(self.dockWidgetContents)
-        self.warning.setGeometry(QtCore.QRect(30, 50, 161, 20))
-        self.warning.setTextFormat(QtCore.Qt.PlainText)
-        self.warning.setObjectName("warning")
+        self.gridLayout = QtGui.QGridLayout(self.dockWidgetContents)
+        self.gridLayout.setObjectName("gridLayout")
         self.btnConnect = QtGui.QPushButton(self.dockWidgetContents)
-        self.btnConnect.setGeometry(QtCore.QRect(20, 10, 78, 27))
         self.btnConnect.setObjectName("btnConnect")
+        self.gridLayout.addWidget(self.btnConnect, 0, 0, 1, 1)
+        self.groupBox = QtGui.QGroupBox(self.dockWidgetContents)
+        self.groupBox.setTitle("")
+        self.groupBox.setObjectName("groupBox")
+        self.gridLayout.addWidget(self.groupBox, 1, 0, 1, 1)
         QgsWpsDockWidget.setWidget(self.dockWidgetContents)
 
         self.retranslateUi(QgsWpsDockWidget)
         QtCore.QMetaObject.connectSlotsByName(QgsWpsDockWidget)
 
     def retranslateUi(self, QgsWpsDockWidget):
-        QgsWpsDockWidget.setWindowTitle(QtGui.QApplication.translate("QgsWpsDockWidget", "QgsWps", None, QtGui.QApplication.UnicodeUTF8))
-        self.btnConnect.setText(QtGui.QApplication.translate("QgsWpsDockWidget", "connect", None, QtGui.QApplication.UnicodeUTF8))
+        QgsWpsDockWidget.setWindowTitle(QtGui.QApplication.translate("QgsWpsDockWidget", "QGIS WPS-Client", None, QtGui.QApplication.UnicodeUTF8))
+        self.btnConnect.setText(QtGui.QApplication.translate("QgsWpsDockWidget", "server connections", None, QtGui.QApplication.UnicodeUTF8))
 
 
 if __name__ == "__main__":
