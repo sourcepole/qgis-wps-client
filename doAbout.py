94ff418ae366eb035327bfad7dd6a5a1a1edd92a
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2012-02-14 07:47:29 +0000

    Datum korrigiert

--- doAbout.py
+++ doAbout.py
@@ -37,7 +37,7 @@ class DlgAbout( QDialog, Ui_dlgAbout ):
     
         # setup labels
         ver = '0.7.5'
-        dt = "2012-02-10"
+        dt = "2012-02-14"
         self.lblVersion.setText( self.tr( "Version: %1" ).arg( ver ) )
         self.lblDate.setText( self.tr( "Date: %1" ).arg( dt ) )
     
