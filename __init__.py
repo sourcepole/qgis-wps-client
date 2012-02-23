6e3bf0c51b95a7f4285861e7150d80942abb48f5
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2012-02-23 09:12:40 +0000

    Italienische Übersetzung eingeführt und auf 0.7.8 gesetzt

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ def description():
   return "Client for Web Processing Services"
 
 def version():
-  return "0.7.7"
+  return "0.7.8"
 
 def qgisMinimumVersion():
   return "1.4"  
--- doAbout.py
+++ doAbout.py
@@ -36,7 +36,7 @@ class DlgAbout( QDialog, Ui_dlgAbout ):
         self.tabWidget.setCurrentIndex( 0 )
     
         # setup labels
-        ver = '0.7.7'
+        ver = '0.7.8'
         dt = "2012-02-23"
         self.lblVersion.setText( self.tr( "Version: %1" ).arg( ver ) )
         self.lblDate.setText( self.tr( "Date: %1" ).arg( dt ) )
