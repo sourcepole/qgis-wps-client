322bc50663d2047202e07fd57a362c5dafbf22be
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2012-10-10 18:33:49 +0000

    version 0.9.1 and date changed

--- __init__.py
+++ __init__.py
@@ -25,13 +25,13 @@ def description():
   return "Client for Web Processing Services"
 
 def version():
-  return "0.9.0"
+  return "0.9.1"
 
 def qgisMinimumVersion():
   return "1.5"  
 
 def date():
-    return '2012-09-06'
+    return '2012-10-11'
     
 def authorName():
   return "Dr. Horst Düster"
