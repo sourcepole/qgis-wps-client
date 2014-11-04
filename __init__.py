4f3a97b717cacc3423fdaa1d03c2c8d1dc42637d
hdus <hdus@71b34618-ca1c-4a1a-b710-e352b2aca4cd>
2014-11-04 17:23:45 +0000

    version 2.0.12

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ def description():
   return "Client for Web Processing Services"
 
 def version():
-  return "2.0.11"
+  return "2.0.12"
 
 def qgisMinimumVersion():
   return "1.5"  
@@ -32,7 +32,7 @@ def qgisMaximumVersion():
   return "2.99"    
 
 def date():
-    return '2014-10-20'
+    return '2014-11-04'
     
 def email():
     return 'horst.duester@sourcepole.ch'
